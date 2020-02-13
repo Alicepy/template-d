0dcbe82ef6a7e4012f06f4d9399303908bf3ca2d
Alicepy <41033670+Alicepy@users.noreply.github.com>
2020-02-13 15:47:58 +0800

    Update meta.js
    Changed 添加metajs选项

--- meta.js
+++ meta.js
@@ -1,21 +1,28 @@
 module.exports = {
-    "prompts": {
-        "name": {
-            "type"    : "string",
-            "required": false,
-            "message" : "Project name",
-            "default" : "my-project"
+    prompts: {
+        name: {
+            type    : "string",
+            required: false,
+            message : "Project name",
+            default : "my-project"
         },
-        "description": {
-            "type"    : "string",
-            "required": false,
-            "message" : "Project description",
-            "default" : "A new base angularjs project"
+        description: {
+            type    : "string",
+            required: false,
+            message : "Project description",
+            default : "A new base angularjs project"
         },
-        "author": {
-            "type"   : "string",
-            "message": "Author"
+        author: {
+            type   : "string",
+            message: "Author"
+        },
+        test: {
+            type   : "confirm",
+            message: "Use test to lint your code?"
         }
+    },
+    filters: {
+        'test/*':'test'
     },
     "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n bower install\n"
 }
